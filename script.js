e68b6eb76109d802ad428f98d86a2d2228b641c4
Karl Tay <karltay52@gmail.com>
2024-10-11 17:26:28 +0000

    update js

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-// random text to get validation on the auto checker
+// DOMContent loaded. To make sure the page loads before all JS takes place
+document.addEventListener('DOMContentLoaded', function(){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+})
