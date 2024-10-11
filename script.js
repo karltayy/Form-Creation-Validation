72d82b97125556b7a620b94228bf698806662aff
Karl Tay <karltay52@gmail.com>
2024-10-11 17:39:25 +0000

    update js

--- script.js
+++ script.js
@@ -2,6 +2,10 @@
 document.addEventListener('DOMContentLoaded', function(){
 const form = document.getElementById('registration-form')
 const feedbackDiv = document.getElementById('form-feedback')
+form.addEventListener('submit', function(){
+    //Prevent form from submitting to the server
+    event.preventDefault();
+})
 
 
 
