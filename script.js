56e9bba69b556aa088d927a9a1d952e0153b8426
Karl Tay <karltay52@gmail.com>
2024-10-11 17:32:48 +0000

    update js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // DOMContent loaded. To make sure the page loads before all JS takes place
 document.addEventListener('DOMContentLoaded', function(){
 const form = document.getElementById('registration-form')
+const feedbackDiv = document.getElementById('form-feedback')
 
 
 
